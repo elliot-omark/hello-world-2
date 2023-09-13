235b614dcc2080b69e8644b8fa6948de481dfc05
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-09-13 11:21:32 -0700

    cum

---
+++ kjkjij.java
@@ -0,0 +1,29 @@
+public class kjkjij{
+
+    public void run(){
+        int i=0;
+        int k=4;
+        int a=6;
+        int b=18;
+        System.out.println(" ");
+        while(i<15){
+            System.out.println("i is "+ i +" but is less than 10");
+            i=i+1;
+        }
+        while(k>5&&k<10){
+            System.out.println("k is "+ k +" but is greater than 5");
+            k=k+1;
+        }
+        while(a<10&&b<=20){
+            System.out.println("a is "+ a +" but is less than 10");
+            System.out.println("b is "+ b +" but is less than or equal to 20");
+            a=a+1;
+            b=b+1;
+        }
+    }
+
+    public static void main(String[] args){
+        kjkjij hw = new kjkjij();
+        hw.run();
+    }
+}
